b06c1081f5a9c5f6e9d1f27e2b2377ce14028069
andres-linares <andres.linares.971122@gmail.com>
2021-12-08 11:32:03 -0500

    feat: add more things

---
+++ pages/apps.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <header>
+      <div class="logo">
+        MiniApps!
+      </div>
+    </header>
+
+    <main>
+      <nuxt-child />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+header {
+  min-height: 4rem;
+  background: #99d98c;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 2vw;
+  box-shadow: 2px 2px 6px #99d98c45;
+  margin-bottom: 2vh;
+}
+
+header .logo {
+  letter-spacing: 0.06em;
+  font-weight: 700;
+}
+
+main {
+  max-width: 90vw;
+  margin: 0 auto;
+}
+</style>
---
+++ pages/apps/random-number.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>Random Number</h1>
+
+    <form @submit.prevent="onSubmit">
+      <label for="minNumber">Min number</label>
+      <input
+        type="number"
+        id="minNumber"
+        v-model.number="minNumber"
+        :max="maxNumber - 1"
+        required
+      />
+
+      <label for="maxNumber">Max number</label>
+      <input
+        type="number"
+        id="maxNumber"
+        v-model.number="maxNumber"
+        :min="minNumber + 1"
+        required
+      />
+
+      <button type="submit">Generate</button>
+    </form>
+
+    <output>{{ randomNumber }}</output>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RandomNumber",
+  data() {
+    return {
+      minNumber: 1,
+      maxNumber: 9,
+      randomNumber: null
+    };
+  },
+  methods: {
+    onSubmit() {
+      const randomZeroToOne = Math.random();
+
+      const delta = this.maxNumber - this.minNumber;
+      const randomSpace = randomZeroToOne * delta;
+
+      this.randomNumber = Math.round(randomSpace + this.minNumber);
+    },
+  },
+  head() {
+    return {
+      title: 'Random Number'
+    }
+  }
+};
+</script>
+
+<style></style>
