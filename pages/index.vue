63edfd91c43fdf64b005c5d5212925f6d8882f54
andres-linares <andres.linares.971122@gmail.com>
2021-12-08 11:06:12 -0500

    feat: add initial grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,49 @@
 <template>
+  <div>
+    <ul>
+      <li>
+        <article>
+          <h6>Chronometer</h6>
+
+          <p>A simple chronometer with a play and stop button</p>
+        </article>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
+}
+
+article h6 {
+  font-size: 1.25rem;
+  font-weight: 700;
+}
+</style>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Roboto+Mono&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+ul,
+ol {
+  list-style: none;
+}
+
+html,
+body {
+  height: 100%;
+  font-family: 'Nunito', sans-serif;
+}
+</style>
