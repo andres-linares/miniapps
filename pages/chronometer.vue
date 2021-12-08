9de0a808dcbe419c6b19772c4406bcac59fe46b1
andres-linares <andres.linares.971122@gmail.com>
2021-12-08 11:01:18 -0500

    feat: add chronometer app

---
+++ pages/chronometer.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="chronometer">
+    <output class="output">{{ ellapsedTime }}</output>
+
+    <div class="actions">
+      <button @click="start" class="start-button" :disabled="currentTime">
+        <i class="material-icons">play_arrow</i>
+      </button>
+      <button @click="stop" class="stop-button" :disabled="!currentTime">
+        <i class="material-icons">stop</i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chronometer",
+  data() {
+    return {
+      startTime: null,
+      interval: null,
+      currentTime: null,
+    };
+  },
+  computed: {
+    ellapsedTime() {
+      if (!this.currentTime) {
+        return "0:00:00.00";
+      }
+
+      const ellapsedTimeInMs = this.currentTime - this.startTime;
+      const ellapsedTime = ellapsedTimeInMs / 1000;
+
+      let hundreths = Math.floor((ellapsedTimeInMs / 10) % 100);
+      let seconds = Math.floor(ellapsedTime % 60);
+      let minutes = Math.floor((ellapsedTime / 60) % 60);
+      let hours = Math.floor(ellapsedTime / 60 / 60);
+
+      hundreths = hundreths.toString().padStart(2, "0");
+      seconds = seconds.toString().padStart(2, "0");
+      minutes = minutes.toString().padStart(2, "0");
+
+      return `${hours}:${minutes}:${seconds}.${hundreths}`;
+    },
+  },
+  beforeDestroy() {
+    if (this.interval) clearInterval(this.interval);
+  },
+  methods: {
+    start() {
+      this.startTime = performance.now();
+
+      this.interval = setInterval(() => {
+        this.currentTime = performance.now();
+      }, 10);
+    },
+    stop() {
+      clearInterval(this.interval);
+
+      this.startTime = null;
+      this.currentTime = null;
+    },
+  },
+  head() {
+    return {
+      title: "Chronometer",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.chronometer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.output {
+  font-family: "Roboto Mono", monospace;
+  font-size: clamp(1.5rem, 4vw, 3.5rem);
+}
+
+button {
+  cursor: pointer;
+  height: 2.75rem;
+  width: 2.75rem;
+  border-radius: 50%;
+  display: grid;
+  place-items: center;
+  border: 1px solid #dadada;
+  transition: transform 250ms ease-out;
+}
+
+button:disabled {
+  opacity: 0.45;
+  cursor: default;
+}
+
+button:disabled:hover,
+button:disabled:focus {
+  transform: scale(0.95);
+}
+
+button:hover,
+button:focus {
+  transform: scale(1.2);
+}
+
+button:focus {
+  outline-offset: 0.25rem;
+}
+
+.start-button {
+  background: #0e990f;
+  color: #fff;
+}
+
+.stop-button {
+  background: #B8030E;
+  color: #fff;
+}
+
+.actions {
+  margin-top: 2.5vh;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito:wght@700&family=Roboto+Mono&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Nunito:wght@500&display=swap");
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: grid;
+  place-items: center;
+}
+</style>
