3742d5ded7f7abba03960770f18409848375819a
andres-linares <andres.linares.971122@gmail.com>
2021-12-08 16:57:57 -0500

    feat: add pixels filler

---
+++ pages/apps/pixels-filler.vue
@@ -0,0 +1,75 @@
+<template>
+  <div ref="canvas">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PixelsFiller',
+  data() {
+    return {
+      interval: null,
+      userWantsToScroll: false
+    }
+  },
+  created() {
+    this.$store.commit('setTitle', this.$metaInfo.title);
+  },
+  mounted() {
+    let lastScroll = 0;
+
+    document.addEventListener('scroll', () => {
+      const currentScroll = window.scrollY;
+
+      if (lastScroll > currentScroll) {
+        this.userWantsToScroll = true;
+      } else {
+        this.userWantsToScroll = false;
+      }
+
+      lastScroll = currentScroll;
+    });
+
+    this.interval = setInterval(() => {
+      const spans = [];
+
+      const totalSpans = this.$refs.canvas.childElementCount;
+      const chunk = totalSpans < 1000 ? 20 : totalSpans < 5000 ? 10 : totalSpans < 10000 ? 5 : 3;
+
+      for (let i = 0; i < chunk; i++) {
+        const span = document.createElement('span');
+
+        const h = 110;
+        const s = Math.round(Math.random() * 100);
+        const l = Math.round(Math.random() * 100);
+
+        span.style.background = `hsl(${h}, ${s}%, ${l}%)`;
+
+        spans.push(span);
+      }
+
+      this.$refs.canvas.append(...spans);
+      if (!this.userWantsToScroll) window.scrollTo({ top: 999999 });
+    }, 10);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+  head() {
+    return {
+      title: 'Pixels Filler'
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(1vmax, 1fr));
+  grid-auto-rows: 1vmax;
+  margin-bottom: 10vh;
+}
+</style>
