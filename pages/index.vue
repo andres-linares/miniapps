98a1dea0e155268ada96ed847b3b72ec21888ea2
andres-linares <andres.linares.971122@gmail.com>
2021-12-08 12:39:15 -0500

    feat: use data

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,65 @@
 <template>
   <div>
     <ul>
-      <li>
-        <article>
-          <h6>Chronometer</h6>
+      <li v-for="app in apps" :key="app.url">
+        <nuxt-link :to="app.url">
+          <article>
+            <img alt="preview">
 
-          <p>A simple chronometer with a play and stop button</p>
-        </article>
-      </li>
+            <main>
+              <h6>{{ app.title }}</h6>
 
-      <li>
-        <article>
-          <h6>Random Number</h6>
-
-          <p>A random number generator with a bottom and upper limit</p>
-        </article>
+              <p>{{ app.description }}</p>
+            </main>
+          </article>
+        </nuxt-link>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: "Index",
+  data() {
+    return {
+      apps: [
+        {
+          title: "Chronometer",
+          description: "A simple chronometer with a play and stop button",
+          url: "/apps/chronometer",
+        },
+        {
+          title: "Random number",
+          description:
+            "A random number generator with a bottom and upper limit",
+          url: "/apps/random-number",
+        },
+        {
+          title: "Random Image",
+          description: "A random image generator for your background",
+          url: "/apps/random-image",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
 ul {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(16rem, 1fr));
+  column-gap: 1rem;
+  row-gap: 1.5rem;
+}
+
+article {
+  background: #eaeaea;
+}
+
+article main {
+  padding: 0.25rem 0.5rem;
 }
 
 article h6 {
